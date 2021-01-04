b3d95ed44bb3c302c1f56b0b6d3fb8af3ffed334
maxminoS <maxminos.1@gmail.com>
2021-01-05 00:05:04 +0700

    Using Typography

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import 'App.css';
+import 'fontsource-roboto';
+
 import QuoteBox from "components/QuoteBox";
 
 class App extends Component {
--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Typography } from "@material-ui/core";
+import { FormatQuote } from "@material-ui/icons";
 
 export default props => (
   <>
     {props.quote
      ? <>
-         <h3>{props.quote.quote}</h3>
-         <p>- {props.quote.author}</p>
+         <FormatQuote fontSize="large" />
+         <Typography variant="h6">{props.quote.quote}</Typography>
+         <Typography variant="subtitle1" align="right">- {props.quote.author}</Typography>
        </>
      : ""}
   </>
