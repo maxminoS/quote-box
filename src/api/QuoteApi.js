78de82ae4d3ea0da0681d623e713e4fbff4c80e5
maxminoS <maxminos.1@gmail.com>
2021-01-05 20:06:03 +0700

    Fixed fetch link

--- src/api/QuoteApi.js
+++ src/api/QuoteApi.js
@@ -1,7 +1,7 @@
 export default {
   getQuote: async function() {
     try {
-      const response = await fetch('https:gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json');
+      const response = await fetch('https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json');
       const quotes = await response.json();
       return quotes;
     } catch (error) {
