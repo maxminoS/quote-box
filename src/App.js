b692632d88fc52633f732b144fdd15945cbcb7b0
maxminoS <maxminos.1@gmail.com>
2021-01-05 18:00:42 +0700

    Switched root Class Component into Function Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
 import { Grid } from "@material-ui/core";
 import 'fontsource-roboto';
 
 import QuoteBox from "components/QuoteBox";
 
-class App extends Component {
-  render() {
-    return (
-      <Grid container
-            justify="center"
-            alignItems="center"
-            style={{ minHeight: "100vh" }}
-      >
-        <Grid item xs={8}>
-          <QuoteBox />
-        </Grid>
+export default () => {
+  return (
+    <Grid container
+          justify="center"
+          alignItems="center"
+          style={{ minHeight: "100vh" }}
+    >
+      <Grid item xs={8}>
+        <QuoteBox />
       </Grid>
-    );
-  }
+    </Grid>
+  );
 }
-
-export default App;
