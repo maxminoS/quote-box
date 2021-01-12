41989f102a702c8a9d334e9246c7e28076f097dd
maxminoS <maxminos.1@gmail.com>
2021-01-13 01:14:21 +0700

    Added snapshot testing for Share component

---
+++ src/components/__test__/Share.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Share from 'components/Share';
+
+it('renders Share component as expected', () => {
+  const share = renderer.create(<Share quote={{
+    "quote": "a quote",
+    "author": "by this"
+  }} />).toJSON();
+  expect(share).toMatchSnapshot();
+});
