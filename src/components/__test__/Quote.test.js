8149d90752db4c3c25fb9a5d0aa43e1c89521223
maxminoS <maxminos.1@gmail.com>
2021-01-13 01:07:28 +0700

    Added snapshot testing for Quote component

--- src/components/__test__/Quote.test.js
+++ src/components/__test__/Quote.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 
 import Quote from 'components/Quote';
 
@@ -11,4 +12,12 @@ it('renders Quote component correctly', () => {
 
   expect(screen.getByRole('heading', { name: /a quote/i })).toBeInTheDocument();
   expect(screen.getByRole('heading', { name: /\- by this/i })).toBeInTheDocument();
+});
+
+it('renders Quote component as expected', () => {
+  const quote = renderer.create(<Quote quote={{
+    "quote": "a quote",
+    "author": "by this"
+  }} />).toJSON();
+  expect(quote).toMatchSnapshot();
 });
