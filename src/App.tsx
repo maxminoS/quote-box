a987e4132bcac97992538ab1556ac1120f476202
maxminoS <maxminos.1@gmail.com>
2021-01-20 23:31:04 +0700

    Added dark mode feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,31 @@
-import React from "react";
-import { Grid } from "@material-ui/core";
+import React, { useState } from "react";
+import { Grid, Paper, IconButton } from "@material-ui/core";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import { Brightness3, WbSunny } from "@material-ui/icons";
 import 'fontsource-roboto';
 
 import QuoteBox from "components/QuoteBox";
 
 export default () => {
+  const [darkMode, setDarkMode] = useState(false);
+  const theme = createMuiTheme({ palette: { type: darkMode ? "dark" : "light" } });
+
   return (
-    <Grid container
-          justify="center"
-          alignItems="center"
-          style={{ minHeight: "100vh" }}
-    >
-      <Grid item xs={8} lg={6} xl={4}>
-        <QuoteBox />
+    <ThemeProvider theme={theme}>
+    <Paper>
+      <Grid container
+            justify="center"
+            alignItems="center"
+            style={{ minHeight: "100vh" }}
+      >
+        <Grid item xs={8} lg={6} xl={4}>
+          <QuoteBox />
+        </Grid>
+        <IconButton onClick={() => setDarkMode(!darkMode)}>
+          { darkMode ? <Brightness3 /> : <WbSunny /> }
+        </IconButton>
       </Grid>
-    </Grid>
+    </Paper>
+    </ThemeProvider>
   );
 }
