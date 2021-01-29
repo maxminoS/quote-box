feb3f10407279738f9f17359d379fa6d965d335e
maxminoS <maxminos.1@gmail.com>
2021-01-29 09:34:33 +0700

    Installed Cypress snapshot

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -2,6 +2,8 @@
 declare namespace Cypress {
   interface Chainable<Subject> {
     // Custom commands to select and click the GET button
-    clickGet(): Chainable<Subject>
+    clickGet(): Chainable<Subject>,
+    // Snapshot command
+    snapshot(value: object): Chainable<Subject>
   }
 }
--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -1,3 +1,5 @@
+require("@cypress/snapshot").register();
+
 Cypress.Commands.add("clickGet", () => {
   cy.get(".MuiButton-label")
     .contains("GET").click();
